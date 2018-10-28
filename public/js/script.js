1f9e6d8f44ab2789bb6978f356f2689b65b32fd6
Clement-76 <clement.patigny@gmail.com>
2018-10-28 23:39:46 +0100

    styles section réservation + affichage des marqueurs

--- public/js/script.js
+++ public/js/script.js
@@ -3,9 +3,33 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: 48.859444,
-            lng: 2.366209
+            lat: 45.756327,
+            lng: 4.853837
         },
-        zoom: 16
+        zoom: 15
     });
 }
+
+get("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=a443018b15d69f18cf8c629825d3a24a12bb79f3", function (response) {
+    var stations = JSON.parse(response);
+    stations.forEach(function (station) {
+        
+        var icon = "";
+        if (station.status === 'OPEN') {
+            icon = "/public/images/biker_green.png";
+        } else {
+            icon = "/public/images/biker_red.png";
+        }
+
+        var marker = new google.maps.Marker({
+            position: station.position,
+            map: map,
+            title: station.name,
+            icon: icon
+        });
+        
+        marker.addListener("click", function () {
+            
+        })
+    })
+})
