c34b6726bcad2ebfda8bda37e9a2ce1ac55cd609
Clement-76 <clement.patigny@gmail.com>
2018-10-29 16:02:36 +0100

    affichage des données de la station sélectionnée + ajout d'évènements formulaire de réservation + fleches slider en html/css

---
+++ public/js/booking.js
@@ -0,0 +1,17 @@
+var selectedStation = {
+    
+}
+
+var bookingForm = {
+    previousStep: function () {
+        
+    },
+    
+    nextStep: function () {
+        
+    },
+    
+    book: function () {
+        
+    }
+}
--- public/js/script.js
+++ public/js/script.js
@@ -6,14 +6,15 @@ function initMap() {
             lat: 45.756327,
             lng: 4.853837
         },
-        zoom: 15
+        zoom: 15,
+        gestureHandling: 'greedy'
     });
 }
 
 get("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=a443018b15d69f18cf8c629825d3a24a12bb79f3", function (response) {
     var stations = JSON.parse(response);
     stations.forEach(function (station) {
-        
+
         var icon = "";
         if (station.status === 'OPEN') {
             icon = "/public/images/biker_green.png";
@@ -25,11 +26,37 @@ get("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=a443018b15d69
             position: station.position,
             map: map,
             title: station.name,
-            icon: icon
+            icon: icon,
+            click: function (event) {
+                map.setCenter(station.position);
+                map.setZoom(20);
+
+                if (station.status === 'OPEN') {
+                    $("#state_station").text("Station ouverte");
+                    $("#book").css("visibility", "visible");
+
+                    $("#book").on("click", function () {
+                        $("#step_one").hide();
+                        $("#step_two").show(0);
+                        $("#step_two").css("right", "0");
+                    })
+
+                    $("#step_two h3 i").on("click", function () {
+                        $("#step_two").css("right", "-120%");
+                        $("#step_two").delay(500).hide(0);
+                        $("#step_one").delay(500).show(0);
+                    })
+                } else {
+                    $("#state_station").text("Station fermée");
+                }
+
+                $("#name_station").text(station.name);
+                $("#address_station").text(station.address);
+                $("#available_bikes").text(station.available_bikes);
+                $("#available_bike_stands").text(station.available_bike_stands);
+            }
         });
-        
-        marker.addListener("click", function () {
-            
-        })
+
+        marker.addListener("click", marker.click);
     })
 })
