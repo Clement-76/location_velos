ed6a35ab0e344be87a04b709906a945c542ce07e
Clement-76 <clement.patigny@gmail.com>
2018-11-03 00:08:56 +0100

    Le formulaire de réservation apparait desormais uniquement une fois une station selectionnée

--- public/js/booking.js
+++ public/js/booking.js
@@ -69,6 +69,8 @@ var booking = {
     },
 
     setInformations: function (station) {
+        $(".booking_form").show("slow");
+        
         if (this.actualStep === 2) {
             this.previousStep();
         }
