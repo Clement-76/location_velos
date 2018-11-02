ee9670dacf268fec787d4fd93fb8812a6003ed11
Clement-76 <clement.patigny@gmail.com>
2018-11-02 01:14:00 +0100

    Réservation + affichage de l'état de la réservation

--- public/js/booking.js
+++ public/js/booking.js
@@ -3,11 +3,51 @@ var booking = {
     init: function () {
         this.actualStep = 1;
         $(".booking_form form").on("submit", booking.book);
-        
+        this.autocompletion();
+        this.showBooking();
+    },
+
+    showBooking: function () {
+        if (sessionStorage.getItem("time")) {
+            $("#booking_state").html("Vélo réservé à la station \"" + sessionStorage.getItem("reservedStation") + "\" par " + localStorage.getItem("firstName") + " " + localStorage.getItem("name") + "<br>Temps restant : <span id='remaining_time'></span>");
+            booking.remainingTime();
+        }
+    },
+
+    remainingTime: function () {
+        //twenty minutes in ms
+        var twentyMinutes = 1200000;
+
+        var chrono = setInterval(function () {
+            //Time spent since booking
+            var timeSpent = Date.now() - Number(sessionStorage.getItem("time"));
+            
+            var remainingMs = twentyMinutes - timeSpent;
+            var remainingMinutes = Math.floor(remainingMs / 1000 / 60);
+            
+            // % 60 to substract the minutes to the seconds
+            var remainingSeconds = Math.floor((remainingMs / 1000) % 60);
+            
+            if (String(remainingSeconds).length === 1) {
+                remainingSeconds = "0" + remainingSeconds;
+            }
+            
+            if (timeSpent < twentyMinutes) {
+                $("#remaining_time").text(remainingMinutes + "min " + remainingSeconds + "s");
+            } else {
+                $("#booking_state").html("");
+                sessionStorage.removeItem("time");
+                sessionStorage.removeItem("reservedStation");
+                clearInterval(chrono);
+            }
+        }, 1000);
+    },
+
+    autocompletion: function () {
         //if name and firstName are registered in localStorage
         if (localStorage.getItem("name") && localStorage.getItem("firstName")) {
-            $("#name").val(localStorage.getItem("name"));
-            $("#first_name").val(localStorage.getItem("firstName"));
+            document.getElementById("name").value = localStorage.getItem("name");
+            document.getElementById("first_name").value = localStorage.getItem("firstName");
         }
     },
 
@@ -30,12 +70,18 @@ var booking = {
         if (this.actualStep === 2) {
             this.previousStep();
         }
-        
+
         this.selectedStation = station;
-        
+
         $(".name_station").text(station.name);
         $("#address_station").text(station.address);
-        $("#available_bikes").text(station.available_bikes);
+
+        if (sessionStorage.getItem("reservedStation") && sessionStorage.getItem("reservedStation") === station.name) {
+            $("#available_bikes").text(station.available_bikes - 1 + " (Vous avez 1 vélo de réservé)");
+        } else {
+            $("#available_bikes").text(station.available_bikes);
+        }
+
         $("#available_bike_stands").text(station.available_bike_stands);
     },
 
@@ -52,18 +98,18 @@ var booking = {
         var emptyRegex = /^[\s]*$/;
         booking.name = $("#name").val();
         booking.firstName = $("#first_name").val();
-        
+
         //checking if form elements are not empty
         booking.check(canvas.isEmpty, $("#canvas"));
         booking.check(emptyRegex.test(booking.name), $("#name"));
         booking.check(emptyRegex.test(booking.firstName), $("#first_name"));
 
         if (canvas.isEmpty === false && emptyRegex.test(booking.name) === false && emptyRegex.test(booking.firstName) === false) {
-            localStorage.clear();
             localStorage.setItem("name", booking.name);
             localStorage.setItem("firstName", booking.firstName);
-            
-//            sessionStorage.setItem("reservedStation", booking.selectedStation);
+
+            sessionStorage.setItem("reservedStation", booking.selectedStation.name);
+            sessionStorage.setItem("time", Date.now());
         }
     }
 }
